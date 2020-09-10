d9b0b37d11a0bb111bf76e2a387a0832449d6066
Sneha Gupta <ms.snehagupta2009@gmail.com>
2020-09-10 21:15:43 +0530

    added data fetch logic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const   express     = require("express"),
-        app         = express(),
-        mongoose    = require("mongoose");
+const express   = require("express"),
+    app         = express(),
+    mongoose    = require("mongoose");
+
+//Deprecation warning options
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 
 
 mongoose.connect("mongodb://localhost/iAmFeelingLucky")
@@ -11,7 +16,26 @@ const myQuoteSchema = new mongoose.Schema({
 
 const MyQuote = mongoose.model("MyQuote", myQuoteSchema);
 
+app.get("/", (req, res) => {
+    res.redirect("/iamfeelinglucky");
+});
 
-app.listen(3000, function(){
+app.get("/iamfeelinglucky", (req, res) => {
+    //generate id randomly
+    let randomId = Math.floor(Math.random() * 12) + 1;
+    //get quote using generated id
+    MyQuote.find({ "id": randomId }, (err, foundQuote) => {
+        console.log(foundQuote);
+        let quote = "You're off to great places, today is your day!";
+        if (!err) {
+            quote = foundQuote[0].quotes;
+        }
+        res.send({ quote: quote });
+    });
+});
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
     console.log("iAmFeelingLucky Server Started")
 });
